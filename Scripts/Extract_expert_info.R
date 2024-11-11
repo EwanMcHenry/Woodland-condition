15fc94fc3ac5e3a0e2719225bdf3f76c046bc1f4
EwanMcHenry <ewan.mchenry@outlook.com>
2024-11-11 17:10:08 +0000

    decriptions
    took out indicator name info, moved to its own script

--- Scripts/Extract_expert_info.R
+++ Scripts/Extract_expert_info.R
@@ -14,6 +14,7 @@ library(compiler)
 
 ## respondant name
 respondant.name <-  function(string = list.files(forms.direct)[1]){
+  # Extracts the respondent's name from a given file name by identifying the text preceding a specific pattern (" - give your expert").
   pattern <- " - give your expert"
   string1 <- string
   # Extract the part of the string preceding the pattern
@@ -22,6 +23,9 @@ respondant.name <-  function(string = list.files(forms.direct)[1]){
 
 # cell that contains a defined pattern
 matching_cell_index <- function(tibble_data, pattern) {
+  # Finds the row and column indices of the first cell in a tibble or data frame that contains a value matching a specified pattern. 
+  # Returns the indices or an error message if more than one match is found.
+  
   matching_cells <- tibble_data %>%
     mutate(across(everything(), as.character)) %>%
     rowid_to_column() %>%
@@ -39,58 +43,13 @@ matching_cell_index <- function(tibble_data, pattern) {
   }
 }
 
-
 # INITIAL CURATION + PREP -------------------------------------------------
 
-## a helper df to match different name versions of the same indicator ----
+## Create ind.matcher a helper df to match different name versions of the same indicator ----
 
 all_sheets <- excel_sheets(paste0(forms.direct,list.files(forms.direct)[1]))
-vf_sheets <- all_sheets[-length(all_sheets)]
-
-ind.matcher.df <- data.frame(sheet_name = vf_sheets) # the names of each excel sheet
-### whats written at the top of each sheet ----
-ind.matcher.df$indicator_name <- c("Number of tree size classes (age proxy)",
-                                   "Proportion of the tree and shrub canopy that is native",
-                                   "Vertical structure: Number of tree and shrub canopy layers present",
-                                   "Number of native tree and shrub species",
-                                   "Invasive plant species presence and cover",
-                                   "Number deadwood classes present (of 3 potential classes)  across 4 plot-quarters",
-                                   "Number of ancient/veteran trees per ha",
-                                   "Area of woodland surrounding the plot",
-                                   "Number of regeneration classes present",
-                                   "Amount of potential annual growth removed",
-                                   "Presence of tree disease and rapid mortality",
-                                   "Number of 'positive indicator' plants per plot (10m radius circle)",
-                                   "Number of top-height classes present",
-                                   "Cover of anthropogenic damage")
-### whats written for that indicator in teh weights sheet ----
-ind.matcher.df$weight.name <- c("Tree Age distribution" ,"Canopy nativeness (all layers)" ,"Vertical structure" ,
-                                "Native tree and shrub species number" , "Invasive plants" ,
-                                "Deadwood", "Veteran trees",  "Woodland extent", "Tree regeneration", 
-                                "Herbivore impact", "Tree health", "Ground flora" , "Horizontal complexity", 
-                                "Anthropogenic damage")
-# whats needed to match in the extractio nprocess to find locations of things in excel sheets
-ind.matcher.df$indicator_name_topof_sheet <- ind.matcher.df$indicator_name
-ind.matcher.df$indicator_name_topof_sheet[4] <- "Number of native tree and shrub species"
-ind.matcher.df$indicator_name_topof_sheet[8] <- "Extent/ area of woodland"
-ind.matcher.df$indicator_name_topof_sheet[9] <- "Tree regeneration"
-ind.matcher.df$indicator_name_topof_sheet[10] <- "Herbivore impact"
-ind.matcher.df$indicator_name_topof_sheet[11] <- "Tree disease and rapid mortality"
-ind.matcher.df$indicator_name_topof_sheet[13] <- "Horizontal complexity (structural mosaics across a wood)"
-ind.matcher.df$indicator_name_topof_sheet[14] <- "Anthropogenic damage"
-
-### whats written on the axis title for that indicator's sheet.... wow, this is exhausting! ----
-ind.matcher.df$ind.axis.title <- NA
-for (indicator_num in 1:length(vf_sheets)){
-  ## indicator axis.title
-  data_val_func <- read_excel(paste0(forms.direct,list.files(forms.direct)[1]), sheet = indicator_num) # load data for this indicator:respondent
-  
-  axis.title.loc <-  matching_cell_index(data_val_func, 
-                                         pattern = "the table below to describe that typical relationship") + 
-    c(3,0) 
-  ind.matcher.df$ind.axis.title[indicator_num] <- data_val_func[axis.title.loc$rowid[1], axis.title.loc$column[1]] %>% as.character()
-}
-
+vf_sheets <- all_sheets[-length(all_sheets)] # the names of each excel sheet
+source("Scripts\\indicator_matcher.R") # loads data to match different versions of indicator names
 
 #-----------------------------------------------------
 #  EXTRACT DATA FROM EXCEL SHEETS ----------------------------------------------

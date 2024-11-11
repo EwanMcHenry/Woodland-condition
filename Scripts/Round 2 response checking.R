02647a6da8b582c8bf508fac8890c577a7dc5112
EwanMcHenry <ewan.mchenry@outlook.com>
2024-11-11 10:56:12 +0000

    checking outputs of different rounds where identical

--- Scripts/Round 2 response checking.R
+++ Scripts/Round 2 response checking.R
@@ -27,11 +27,35 @@ r2.ind.matcher.df <- ind.matcher.df
 r2.has_completed <- has_completed
 r2.just.one.df <- just.one.df
 
+
+# which dont change? no need to have separate data for each round
+# check if r1.ind.matcher.df the same as r2.ind.matcher.df
+identical(r1.ind.matcher.df, r2.ind.matcher.df)
+
+# check that the general structure of the data is the same between rounds
+names(r1.df) == names(r2.df)
+names(r1.completed_summary) == names(r2.completed_summary)
+names(r1.has_completed) == names(r2.has_completed)
+names(r1.just.one.df) == names(r2.just.one.df)
+
+#
+
+# add round indicator and combine data
+r1.df$round <- 1
+r2.df$round <- 2
+
+df <- rbind(r1.df, r2.df)
+
+
+
 str(df)
 str(completed_summary)
 str(ind.matcher.df)
 str(has_completed)
 str(just.one.df)
+
+
+
 # table of respondents participation in each round
 
 # assess which parts of the form were changed

4492daae718ce506005267d34487d8fd901a9a7c
EwanMcHenry <ewan.mchenry@outlook.com>
2025-09-10 16:34:01 +0100

    change names

--- Render WECA report.R
+++ Render WECA report.R
@@ -6,20 +6,32 @@ dir.create("outputs/WECA reports", showWarnings = FALSE)
 
 # Define your sites
 sites <- list(
+  ## Beechy Wood ----
+  list(
+    site_name = "Beechy Wood",
+    zone_name = "1",
+    site_survey_folderpath = "Data\\Field Test\\Beechy Wood, Sussex - Corsican Pine PAWS\\Z01\\",
+    site_survey_filename = "WCA - Beechy Wood - Field test 22.05.25.xlsx",
+    site_survey_habitat_type = "base_rich_lowland"
+  ),
+  
+  ## Ladypark Wood ----
   list(
     site_name = "Ladypark Wood",
     zone_name = "1",
-    site_survey_folderpath = "Data\\Field Test\\Ladypark Wood, Wye valley - Non-intervention 70yrs broadleaf SSSI ASNW\\",
+    site_survey_folderpath = "Data\\Field Test\\Ladypark Wood, Wye valley - Non-intervention 70yrs broadleaf SSSI ASNW\\Z01\\",
     site_survey_filename = "WCA - Lady park wood - Field test - 12.06.2025.xlsx",
     site_survey_habitat_type = "base_rich_lowland"
-    ),
+  ),
+  ## Inverliever, Argyll ----
   list(
     site_name = "Inverliever, Argyll",
     zone_name = "1",
-    site_survey_folderpath = "Data\\Field Test\\Inverliever, Argyll - Conifer clearfell FLS\\",
+    site_survey_folderpath = "Data\\Field Test\\Inverliever, Argyll - Conifer clearfell FLS\\Z01\\",
     site_survey_filename = "WCA - Inverliever - Field test 06.06.25.xlsx",
     site_survey_habitat_type = "acidic_upland"
   ),
+  ## Harrisons Wood, Louth, Lincs ----
   list(
     site_name = "Harrisons Wood",
     zone_name = "Z001",
@@ -51,7 +63,12 @@ sites <- list(
   
 )
 
+
+# render - 
 for (site in sites) {
+  # Set the switch value here (TRUE to embed, FALSE to link)
+  site$embed_photos <- FALSE
+  site$include_photos <- T
   
   filename <- paste0("WECA report - ", site$site_name, ", Zone ", site$zone_name, ".html")
   
